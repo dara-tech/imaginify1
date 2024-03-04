1e31903d1490736fb1b72585cfeb9be1337e9fe2
Dara <97618988+dara-tech@users.noreply.github.com>
2024-03-04 17:14:44 +0700

    complete app

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,61 +1,102 @@
-// import { Collection } from "@/components/shared/Collection"
-// import { navLinks } from "@/constants"
-// import { getAllImages } from "@/lib/actions/image.actions"
-// import Image from "next/image"
-// import Link from "next/link"
+import { Collection } from "@/components/shared/Collection"
+import { navLinks } from "../../../constants"
+import { getAllImages } from "@/lib/actions/image.actions"
+import Image from "next/image"
+import Link from "next/link"
 
-// const Home = async ({ searchParams }: SearchParamProps) => {
-//   const page = Number(searchParams?.page) || 1;
-//   const searchQuery = (searchParams?.query as string) || '';
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || '';
 
-//   const images = await getAllImages({ page, searchQuery})
+  const images = await getAllImages({ page, searchQuery})
 
-//   return (
-//     <>
-//       <section className="home">
-//         <h1 className="home-heading">
-//           Unleash Your Creative Vision with Imaginify
-//         </h1>
-//         <ul className="flex-center w-full gap-20">
-//           {navLinks.slice(1, 5).map((link) => (
-//             <Link
-//               key={link.route}
-//               href={link.route}
-//               className="flex-center flex-col gap-2"
-//             >
-//               <li className="flex-center w-fit rounded-full bg-white p-4">
-//                 <Image src={link.icon} alt="image" width={24} height={24} />
-//               </li>
-//               <p className="p-14-medium text-center text-white">{link.label}</p>
-//             </Link>
-//           ))}
-//         </ul>
-//       </section>
-
-//       <section className="sm:mt-12">
-//         <Collection 
-//           hasSearch={true}
-//           images={images?.data}
-//           totalPages={images?.totalPage}
-//           page={page}
-//         />
-//       </section>
-//     </>
-//   )
-// }
-
-// export default Home
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
-
-const Home = () => {
   return (
-    <div>
+    <>
+      <section className='home shadow-lg'>
+      <h1 className='home-heading'> Unleah your Creative Vision with <span className=" bg-gradient-to-r from-pink-500 to bg-yellow-500 rounded-lg text-white shadow-lg"> Dara Ai </span></h1>
+      <ul className='flex-center w-full gap-20'>
+        {navLinks.slice(1,5).map((link)=>(
+          <Link
+          key={link.route}
+          href={link.route}
+          className='flex-center flex-col gap-2'
+          
+          >
+          <li className='flex-center w-fit rounded-full bg-slate-100 p-4 shadow-lg'>
+            <Image src={link.icon}
+            alt='image' width={24} height={24}/>
 
-      {/* <UserButton afterSignOutUrl='/'/> */}
-    </div>
+          </li>
+          <p className='p-14-meduim text-center text-white'>{link.label}</p>
+          </Link>
 
+        ))}
+
+      </ul>
+    </section>
+
+      <section className="sm:mt-12">
+        <Collection 
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
+      </section>
+    </>
   )
 }
 
 export default Home
+// import { UserButton } from '@clerk/nextjs'
+// import React from 'react'
+// import { navLinks } from '../../../constants'
+// import Link from 'next/link'
+// import Image from 'next/image'
+// import { Collection } from '@/components/shared/Collection'
+// import { getAllImages } from '@/lib/actions/image.actions'
+
+// const Home =  async ({searchParams}: SearchParamProps) => {
+//   const page = Number(searchParams?.page) || 1;
+//   const searchQuery =(searchParams?.query as string) || '';
+//   const images = await getAllImages({page, searchQuery});
+//   return (
+//     <>
+//     <section className='home shadow-lg'>
+//       <h1 className='home-heading'> Unleah your Creative Vision with <span className=" bg-gradient-to-r from-pink-500 to bg-yellow-500 rounded-lg text-white shadow-lg"> Dara Ai </span></h1>
+//       <ul className='flex-center w-full gap-20'>
+//         {navLinks.slice(1,5).map((link)=>(
+//           <Link
+//           key={link.route}
+//           href={link.route}
+//           className='flex-center flex-col gap-2'
+          
+//           >
+//           <li className='flex-center w-fit rounded-full bg-slate-100 p-4 shadow-lg'>
+//             <Image src={link.icon}
+//             alt='image' width={24} height={24}/>
+
+//           </li>
+//           <p className='p-14-meduim text-center text-white'>{link.label}</p>
+//           </Link>
+
+//         ))}
+
+//       </ul>
+//     </section>
+//     <section className='sm:mt-12'>
+//       <Collection
+//       hasSearch={true}
+//       images={images?.data}
+//       totalPages={images?.totalPage}
+//       page={page}
+      
+//       />
+
+//     </section>
+//     </>
+
+//   )
+// }
+
+// export default Home
